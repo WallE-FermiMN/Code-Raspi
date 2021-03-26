c4607c9f0cf7c940cc2b38945b7ff22272d59b53
francesco <fra2312fra@gmail.com>
2021-03-26 15:35:45 +0100

    Final cose v2

--- serial_comm/examples/simple_test.rs
+++ serial_comm/examples/simple_test.rs
@@ -9,17 +9,21 @@ fn main(){
     std::thread::spawn(move || serial_comm::init(rec,snd2));
     std::thread::sleep(Duration::from_millis(500));
     let c2 = serial_comm::Command::ClockSync;
-    let c3 = serial_comm::Command::EaseDc(EaseDcCommand{ time: Duration::from_millis(345), values: (10, -20) });
-    let c4 = serial_comm::Command::EaseServo(EaseServoCommand{
-        time: Duration::from_millis(5232),
-        channel: 12,
-        val: 4000
-    });
     let c5 = serial_comm::Command::ShutdownThreads;
     snd.send(c2);
-    snd.send(c3);
-    snd.send(c4);
     std::thread::sleep(Duration::from_millis(500));
+    loop {
+        let c3 = serial_comm::Command::EaseDc(EaseDcCommand{ time: Duration::from_millis(345), values: (10, -20) });
+        let c4 = serial_comm::Command::EaseServo(EaseServoCommand{
+            time: Duration::from_millis(5232),
+            channel: 12,
+            val: 4000
+        });
+        snd.send(c3);
+        std::thread::sleep(Duration::from_millis(250));
+        snd.send(c4);
+        std::thread::sleep(Duration::from_millis(250));
+    }
     snd.send(c5);
     std::thread::sleep(Duration::from_millis(500));
 }
